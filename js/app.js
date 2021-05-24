bf42c13d6d1b833f23391f7c92b0270bdfe8b68c
alaabaraka <alaabarakeh96@gmail.com>
2021-05-24 19:41:58 +0300

    coding

---
+++ js/app.js
@@ -0,0 +1,64 @@
+'use strict';
+
+console.log('wellcome to my web siet')
+
+let Name= prompt('whats your name')
+
+
+
+
+console.log(Name);
+
+
+
+let feeling= confirm('are you good')
+if (feeling === true) {
+    console.log(yes);
+}
+else {
+    console.log(no)
+}
+
+let favuriteDish= confirm('do you love MANSAF')
+if (favuriteDish=== true) {
+    console.log(yes);
+}
+else {
+    console.log(no)
+}
+
+let hobbeis= confirm('do you love dancing')
+if (hobbeis=== true) {
+    console.log(yes);
+}
+else {
+    console.log(no)
+}
+
+let  sports= confirm('do you like football')
+if (sports=== true) {
+    console.log(yes);
+}
+else {
+    console.log(no)
+}
+
+let color= confirm('do you love color red')
+if (color=== true) {
+    console.log(yes);
+}
+else {
+    console.log(no)
+}
+
+
+let Name= prompt('whats your name')
+
+
+
+
+console.log(Name);
+
+
+
+
