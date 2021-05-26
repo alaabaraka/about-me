ce29c06296636c6bbc89838bb0cf89b92862d740
alaabaraka <alaabarakeh96@gmail.com>
2021-05-26 20:59:47 +0300

    lab2

--- jscript/app.js
+++ jscript/app.js
@@ -1,57 +1,34 @@
-'use strict';
-console.log('wellcome to my web siet');
-let Name= prompt('whats your name');
-console.log(Name);
-function feeling() {
-let feeling= confirm('are you good');
-if (feeling === true) {
-    console.log(yes);
-}
-else {
-    console.log(no);
-}
-}
-feeling();
- function favuriteDish() {
-let favuriteDish= confirm('do you love MANSAF')
-if (favuriteDish=== true) {
-    console.log(yes);
-}
-else {
-    console.log(no)
-}
- }
- favuriteDish();
-function hobbeis() {
-let hobbeis= confirm('do you love dancing')
-if (hobbeis=== true) {
-    console.log(yes);
-}
-else {
-    console.log(no)
-}
-}
-hobbeis();
-function sports() {
-let  sports= confirm('do you like football')
-if (sports=== true) {
-    console.log(yes);
-}
-else {
-    console.log(no)
-}
-}
-sports();
-function color() {
-let color= confirm('do you love color red')
-if (color=== true) {
-    console.log(yes);
-}
-else {
-    console.log(no)
-}
-}
-color();
-let Name= prompt('whats your name')
-console.log(Name);
+//
+/*
+1. Welcome Message for the User
+2. Ask the user is your favourite book literture?
+3. Keep asking
+
 */
+
+alert(' Welcome to my website');
+
+let favouriteBook=prompt (' is your favourite book literture ?');
+
+favouriteBook.toLowerCase();
+
+if (favouritebook !== 'yes' || favouritebook !== 'y') {
+  alert('correct,great job');
+} else if (favouritebook !== 'no' || favouritebook !== 'n') {
+  alert('sorry thats wrong');
+} else
+
+  alert('Please only answer  yes/y no/n ');
+
+
+
+
+var hearts = prompt('How do you rate our Website 1-5?');
+
+if (hearts > 5) {
+  hearts = 5;
+}
+for (var i = 0; i < hearts; i++) {
+  console.log(i);
+  document.write('<img src="https://qtxasset.com/styles/breakpoint_xl_880px_w/s3/fiercebiotech/1607691764/connor-wells-534089-unsplash.jpg/connor-wells-534089-unsplash.jpg?IxnhKzf6LZYze4g.sUsGbEiQZd5tCaJN&itok=gmQPxIfy" width="80px">');
+}
