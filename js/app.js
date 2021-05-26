5c185eb74c97cd9d3901cbbbb0c120a5c3d48a6a
Ala' baroud <83537397+alaabaroud@users.noreply.github.com>
2021-05-26 15:53:21 +0300

    Revert "lab04"

--- js/app.js
+++ js/app.js
@@ -1,69 +1,6 @@
 'use strict';
 
-console.log('wellcome to my web siet');
-
-let Name= prompt('whats your name');
-
-
-
-
-console.log(Name);
-
-
-function feeling() {
-let feeling= confirm('are you good');
-if (feeling === true) {
-    console.log(yes);
-}
-else {
-    console.log(no);
-}
-}
-feeling();
-
- function favuriteDish() {
-let favuriteDish= confirm('do you love MANSAF')
-if (favuriteDish=== true) {
-    console.log(yes);
-}
-else {
-    console.log(no)
-}
- }
- favuriteDish();
-
-function hobbeis() {
-
-let hobbeis= confirm('do you love dancing')
-if (hobbeis=== true) {
-    console.log(yes);
-}
-else {
-    console.log(no)
-}
-}
-hobbeis();
-function sports() {
-let  sports= confirm('do you like football')
-if (sports=== true) {
-    console.log(yes);
-}
-else {
-    console.log(no)
-}
-}
-sports();
-
-function color() {
-let color= confirm('do you love color red')
-if (color=== true) {
-    console.log(yes);
-}
-else {
-    console.log(no)
-}
-}
-color();
+console.log('wellcome to my web siet')
 
 let Name= prompt('whats your name')
 
@@ -74,4 +11,54 @@ console.log(Name);
 
 
 
-*/
+let feeling= confirm('are you good')
+if (feeling === true) {
+    console.log(yes);
+}
+else {
+    console.log(no)
+}
+
+let favuriteDish= confirm('do you love MANSAF')
+if (favuriteDish=== true) {
+    console.log(yes);
+}
+else {
+    console.log(no)
+}
+
+let hobbeis= confirm('do you love dancing')
+if (hobbeis=== true) {
+    console.log(yes);
+}
+else {
+    console.log(no)
+}
+
+let  sports= confirm('do you like football')
+if (sports=== true) {
+    console.log(yes);
+}
+else {
+    console.log(no)
+}
+
+let color= confirm('do you love color red')
+if (color=== true) {
+    console.log(yes);
+}
+else {
+    console.log(no)
+}
+
+
+let Name= prompt('whats your name')
+
+
+
+
+console.log(Name);
+
+
+
+
